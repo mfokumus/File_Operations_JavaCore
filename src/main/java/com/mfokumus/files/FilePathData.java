6c82a1ac27843c8bd35e08521c3952cb7db7994b
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2023-11-09 00:22:29 +0300

    FilePathData, FilePathUrl, Main

--- src/main/java/com/mfokumus/files/FilePathData.java
+++ src/main/java/com/mfokumus/files/FilePathData.java
@@ -1,2 +1,28 @@
-package com.mfokumus.files;public class FilePathData {
+package com.mfokumus.files;
+
+import java.io.File;
+import java.util.Date;
+import java.util.UUID;
+
+public class FilePathData {
+
+    // Variables
+    private String id;
+    private String pathFileName;
+    private String pathDirectoryName;
+    private String url;
+    private String path;
+    private File file;
+    private Date systemCreatedDate;
+
+    // Constructor (Parametresiz)
+    public FilePathData() {
+        this.id = UUID.randomUUID().toString();
+        systemCreatedDate = new Date(System.currentTimeMillis());
+        pathFileName = "\\secretkey.txt";
+        // C:\\io\\techcareer\\full_file
+        this.pathDirectoryName = FilePathUrl.MY_FILE_PATH_URL;
+        // C:\\io\\techcareer\\full_file\\secretkey.txt
+
+    }
 }
--- src/main/java/com/mfokumus/files/FilePathUrl.java
+++ src/main/java/com/mfokumus/files/FilePathUrl.java
@@ -1,2 +1,7 @@
-package com.mfokumus.files;public class FilePathUrl {
+package com.mfokumus.files;
+
+public class FilePathUrl {
+    // Const
+
+    public static final String MY_FILE_PATH_URL = "C:\\io\\techcareer\\full_file";
 }
